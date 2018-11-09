812645fe1924a3f7082556fbbaa57b2136ae752d
WastlSepp <40317478+WastlSepp@users.noreply.github.com>
2018-11-09 18:15:02 +0100

    Einführung Feeder

---
+++ src/Feeder/FeederOnvista.java
@@ -0,0 +1,19 @@
+/*
+    @author andygschaider
+    @description: automatisches Abfragen von Aktien von bekannten Indizes von onvista.
+    Klasse Onvista wird dann mit den Aktiennamen gefüttert und zieht die jeweiligen Daten aus dem Netz.
+ */
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+
+//import java.io.IOException;
+//import java.util.Scanner;
+
+public class DbFillerOnvista {
+
+    private String url = "https://www.onvista.de";
+    //Document... IntelliJ macht bei mir keine Autovervollständigung für Jsoup... Jemand einen Plan warum?
+
+}
