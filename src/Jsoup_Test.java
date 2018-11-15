51f057bfd286afc42113f3a688a9647360e4a8dc
WastlSepp <40317478+WastlSepp@users.noreply.github.com>
2018-11-16 00:30:46 +0100

    hotfix

--- src/Jsoup_Test.java
+++ src/Jsoup_Test.java
@@ -18,7 +18,7 @@ public class Jsoup_Test {
 //  --- --- -=- -=- -== -== === === Website Timestamps === === ==- ==- -=- -=- --- ---
 
         WebsiteTimestamps wsts = new WebsiteTimestamps();
-        System.out.println(wsts.getLastModifiedDateFromWebpage("https://www.ariva.de/aktien/"));
+        /*Ariva:Startseite     */ System.out.println(wsts.getLastModifiedDateFromWebpage("https://www.ariva.de/aktien/"));
         /*Onvista:Daimler      */ System.out.println(wsts.getLastModifiedDateFromWebpage("https://www.onvista.de/aktien/Daimler-Aktie-DE0007100000"));
         /*Morningstar:Schaltbau*/ System.out.println(wsts.getLastModifiedDateFromWebpage("http://tools.morningstar.de/de/stockreport/default.aspx?Site=de&id=0P00009QOV&LanguageId=de-DE&SecurityToken=0P00009QOV]3]0]E0WWE$$ALL"));
 
