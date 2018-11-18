b92e9bacad5b87afcf407fc7acc7ecb694a6b14a
tobstar97 <37182531+tobstar97@users.noreply.github.com>
2018-11-18 11:38:21 +0100

    Add files via upload

---
+++ src/WKN_Management.java
@@ -0,0 +1,38 @@
+package Hilfsklassen;
+/**
+ * @author Tobias Heiner
+ * speichert alle ISINs und WKNs in einer HashMap (notwendig, da die linkzusammensetzungen nicht mit wkns funktioniert
+ * später soll die HashMap eventuell über die Feeder Klassen "gefüttert" werden
+ *
+ */
+
+import java.util.HashMap;
+
+public class WKN_Management {
+    /**
+     * Map aller WKN mit zugehöriger ISIN
+     */
+    HashMap<String, String> wm = new HashMap<>();
+
+    /**
+     * fügt ein WKN ISIN Paar hinzu
+     * @param isin
+     * @param wkn
+     */
+    public void add(String isin, String wkn){
+        wm.put(wkn, isin);
+        //wm.put("1", "2");
+    }
+
+
+    /**
+     * liefert die ISIN einer Aktie
+     * @param wkn
+     * @return ISIN einer Aktie
+     */
+    public String getISIN(String wkn){
+        System.out.println(wm.get(wkn));
+        return wm.get(wkn);
+    }
+
+}
