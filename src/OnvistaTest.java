8e01d55add537532ab35b4ba0936ca7ddfec0317
tobstar97 <37182531+tobstar97@users.noreply.github.com>
2018-12-24 11:21:58 +0100

    Update OnvistaTest.java

--- src/OnvistaTest.java
+++ src/OnvistaTest.java
@@ -1,6 +1,6 @@
 /**
  * @author Tobias Heiner
- * @description onvista
+ * @description Feeder für onvista; Ziel: Weitergabe der ISIN der gefundenen Aktien auf onvista an ariva, da man dort die historischen Kurse viel besser auslesen kann
  */
 
 import org.jsoup.Jsoup;
@@ -8,7 +8,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 
-
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -16,20 +16,27 @@ public class OnvistaTest {
 
     String url = "https://www.onvista.de/aktien/aktien-laender/";
     Document doc;
-    Document l;
     ArrayList<String>arlaender = new ArrayList<>();
     ArrayList<String>araktien = new ArrayList<>();
     ArrayList<String>arISIN = new ArrayList<>();
     int anzahl;
+    String cut;
 
 
-
-
+    /**
+     * Connection zur onvista-Seite
+     * @author Tobias Heiner
+     * @throws IOException
+     */
     public void connect()throws IOException{
         doc = Jsoup.connect(url).get();
     }
 
-
+    /**
+     * liest alle Länderlinks aus und speichert sie in der ArrayList arlaender
+     * @author Tobias Heiner
+     * @throws IOException
+     */
     public void laender()throws IOException{
         Element e = doc.getElementsByClass("land-list").first();
         anzahl = e.childNodeSize()/2;
@@ -47,6 +54,12 @@ public class OnvistaTest {
 
     }
 
+    /**
+     * liest die Aktienlinks aus und läuft durch alle Seiten eines Länderlinks
+     * AKtienlinkks werden in der ArrayList araktien gespeichert
+     * @author Tobias Heiner
+     * @throws IOException
+     */
     public void aktienderlaender()throws IOException{
         //System.out.println(arlaender.get(8));
         int siz = arlaender.size();
@@ -62,8 +75,8 @@ public class OnvistaTest {
                 //System.out.println(doc.select("div.BLAETTER_NAVI:nth-child(1) > span:nth-child(1)").text());
 
 
-                String cut = arlaender.get(j);
-                System.out.println(cut);
+                cut = arlaender.get(j);
+                //System.out.println(cut);
                 //String temp = e.child(0).attr("abs:href");
                 //temp = temp.replace(cut, "");
 
@@ -74,25 +87,8 @@ public class OnvistaTest {
                     o++;
                     cut = cut+o;
                     o--;
-                    //ab hier könnte man bis zur line 95 eine eigene Methode schreiben und die dann einfach hier aufrufen
-                    int t = doc.select("td.TEXT").size();
-                    //System.out.println(t);
-                    int i = 0;
-                    while(i < t){       //try catch benötigt (wie unten) -- erledigt
-                        try{
-                            doc = Jsoup.connect(cut).get();
-                            Element meta = doc.select("td.TEXT").get(i);
-                            String table = meta.child(0).attr("abs:href");
-                            System.out.println(table);
-                            araktien.add(table);
-                        }catch (Exception ex){
 
-                        }finally {
-                            i = i+3;
-                        }
-
-
-                    }
+                    aktienDurchlaufen();
 
                     o++;
                     cut = cut.replace(String.valueOf(o), "");
@@ -102,7 +98,7 @@ public class OnvistaTest {
 
             }else{      //wenn es ein LETZTE Seite Element gibt
                 //sucht sich die letzte Seite, speichert den Wert und läuft dann durch die Seiten durch
-                String cut = arlaender.get(j);
+                cut = arlaender.get(j);
                 //System.out.println(cut);
                 String temp = e.child(0).attr("abs:href");
                 temp = temp.replace(cut, "");
@@ -114,26 +110,9 @@ public class OnvistaTest {
                     cut = cut+i;
                     //System.out.println(cut);
                     //hier wird dann die seite nach aktien durchlaufen
-                    //ab hier könnte man bis zur line 136 eine eigene Methode schreiben und die dann einfach hier aufrufen (siehe Kommentar oben, da diese Teile redundant sind)
-                    int t = doc.select("td.TEXT").size();
-                    //System.out.println(t);
-                    int kl = 0;
-                    while(kl < t){
-                        try{
-                            doc = Jsoup.connect(cut).get();
-                            Element meta = doc.select("td.TEXT").get(kl);
-                            //System.out.println();
-                            String tab = meta.child(0).attr("abs:href");
-                            System.out.println(tab);
-                            araktien.add(tab);
-                        }catch (Exception ex){
-                            //System.out.println(ex);
-                        }finally {
-                            kl = kl+3;
-                        }
 
+                    aktienDurchlaufen();
 
-                    }
 
 
                     cut = cut.replace(String.valueOf(i), "");
@@ -149,11 +128,16 @@ public class OnvistaTest {
 
     }
 
-
+    /**
+     * liest die ISIN eines Aktienlinks aus und speichert diese in arISIN
+     * es werden einfach nur die 12 letzen Stellen eines Aktienlinks abgeschnitten und gespeichert
+     * @author Tobias Heiner
+     * @throws IOException
+     */
     public void getISIN()throws IOException{
         int j = araktien.size();
         int i = 0;
-        while(i<j){     //try catch benötigt, da seite eventuell nicht mehr verfügbar -- erledigt
+        while(i<j){     //try catch benötigt, da seite eventuell nicht mehr verfügbar -- erledigt 
             try{
                 String temp = araktien.get(i);
                 temp = temp.substring((temp.length()-12));
@@ -169,14 +153,65 @@ public class OnvistaTest {
         }
     }
 
+    /**
+     * dient zur Visualisierung der Ergebnisse
+     * @author Tobias Heiner
+     */
     public void ausgeben(){
         System.out.println(arISIN.size());
-        System.out.println(arISIN);
+        //System.out.println(arISIN);
+        System.out.println("-----   -----   -----   -----");
+        System.out.println("akitenlinks");
+        System.out.println(araktien.size());
+        //System.out.println(araktien);
+        System.out.println("-----   -----   -----   -----");
+        System.out.println("laenderlinks");
+        System.out.println(arlaender.size());
+        //System.out.println(arlaender);
     }
 
 
+    /**
+     * schreibt in eine .txt Datei die Ergebnisse, damit man nicht immer alle AKtien durchlaufen muss (Zeitersparnis)
+     * @author Tobias Heiner
+     * @throws IOException
+     */
+    public void writ()throws IOException{
+        FileWriter fw = new FileWriter("ausgabeISIN.txt");
+        int i = 0;
+        while(i < arISIN.size()){
+            fw.write(arISIN .get(i));
+            fw.write("\n");
+            //fw.close();
+            i++;
+        }
+        fw.close();
+    }
+
+    /**
+     * durchläuft eine Seite, um alle Links zu den Aktien zu bekommen
+     */
+    public void aktienDurchlaufen(){
+        int t = doc.select("td.TEXT").size();
+        //System.out.println(t);
+        int kl = 0;
+        while(kl < t){
+            try{
+                doc = Jsoup.connect(cut).get();
+                Element meta = doc.select("td.TEXT").get(kl);
+                //System.out.println();
+                String tab = meta.child(0).attr("abs:href");
+                System.out.println(tab);
+                araktien.add(tab);
+            }catch (Exception ex){
+                //System.out.println(ex);
+            }finally {
+                kl = kl+3;
+            }
 
 
+        }
+    }
 
 
 }
