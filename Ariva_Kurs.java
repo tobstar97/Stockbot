5727b6ace20fde47fc76c43cf0403366fcc1c1d5
tobstar97 <37182531+tobstar97@users.noreply.github.com>
2019-03-07 17:33:11 +0100

    Update Ariva_Kurs.java

--- Ariva_Kurs.java
+++ Ariva_Kurs.java
@@ -1,10 +1,11 @@
 package Ariva;
 
 
-
 /**
  * @author Tobias Heiner
  * @description stellt die verbindung zur website ariva.de her und liefert den aktuellen Kurs und Gewinn
+ * @problem ariva block nach einer bestimmten Anzahl von Abrufen in einer gewissen Zeit die IP des Computers
+ * @loesung wir starten eine "Versuchsreihe", um die genauen Parameter rauszufinden, wann ariva die IP blockiert
  */
 
 import org.jsoup.Jsoup;
@@ -22,7 +23,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.util.Scanner;
-
 
 
 public class Ariva_Kurs {
@@ -46,11 +46,10 @@ public class Ariva_Kurs {
      * @param url
      * @throws IOException
      */
-    public void connectKurs(String url) throws IOException{
+    public void connectKurs(String url) throws IOException {
         doc = Jsoup.connect(url).get();
 
     }
-
 
 
     /**
@@ -80,7 +79,7 @@ public class Ariva_Kurs {
      *Liefert den aktuellen Kurs einer Aktie auf ariva
      * @return Kurs als double
      */
-    public double getKurs(){
+    public double getKurs() {
         Element span = doc.select("span[itemprop=price]").first();
         String content;
 
@@ -103,7 +102,7 @@ public class Ariva_Kurs {
      * Liefert den aktuellen gewinn eines Unternehmens auf ariva
      * @return gewinnn als double
      */
-    public double getGewinn(){
+    public double getGewinn() {
         Element meta = doc.getElementsByClass("subtitle level text-linkblue clickCursor").get(3);
         Element table = meta.siblingElements().get(4);
         //Element tb = table.elementSiblingIndex();
@@ -120,7 +119,7 @@ public class Ariva_Kurs {
     /**
      * Liefert die Analysen für 12 Monate
      */
-    public void getAnalyse(){
+    public void getAnalyse() {
         Element meta = doc.getElementsByClass("ag_analysis analysis abstand").first();
 
         Element table = meta.children().first();
@@ -139,25 +138,29 @@ public class Ariva_Kurs {
      * bekommt die links aus der arraylist, die vorher mit der methode kursLink reingeschrieben wurden
      * @throws IOException
      */
-    public void getKursGesamt() throws IOException{
+    public void getKursGesamt() throws IOException {
 
         int i = 0;
         int k = 0;
         double d = 0;
+        //Loesung für das blockier-Problem - Versuch
+        int anzahl = 0;         //Variable, um die Anzahl der Abrufe zu zählen
         String st;
         Element testdate = doc.getElementsByClass("arrow0").get(0);
         String s = testdate.text();
 
 
-        int z=0;
-        while(z<al.size()) {
+        int z = 0;
+        while (z < al.size()) {
+            anzahl++;           //Anzahl der Verbindungen auf die ariva seite
+            System.out.println(anzahl);         //gibt die Anzahl aus
             System.out.println(al.get(z));
             connectKurs(al.get(z));
             //doc = Jsoup.connect("https://www.ariva.de/allianz-aktie/historische_kurse?boerse_id=6&month=2018-10-31&currency=&clean_split=1&clean_split=0&clean_payout=0&clean_bezug=1&clean_bezug=0").get();
             k = doc.getElementsByClass("arrow0").size();
             //System.out.println(k);
 
-            while(i<k) {
+            while (i < k) {
                 //System.out.println("Test");
                 String content;
                 Element meta = doc.getElementsByClass("arrow0").get(i);
@@ -171,7 +174,8 @@ public class Ariva_Kurs {
 
                 d = Double.parseDouble(content);
                 st = date.text();
-                System.out.println(d+"  "+st);
+                System.out.println(d + "  " + st);
+                System.out.println(anzahl);
                 hm.put(st, d);
                 //System.out.println(hm.size());
 
@@ -184,10 +188,7 @@ public class Ariva_Kurs {
         al.clear();
 
 
-
-
     }
-
 
 
     /**
@@ -200,12 +201,12 @@ public class Ariva_Kurs {
      * @return Teilstring, "boerse_id=..."
      * @throws IOException
      */
-    public String haupthandelsplatz (String str)throws IOException{
+    public String haupthandelsplatz(String str) throws IOException {
 
-        try{
+        try {
             //doc = Jsoup.connect("https://www.ariva.de/"+str+"/kurs").get();
-            doc = Jsoup.connect(str+"/kurs").get();
-        }catch(ConnectException e){
+            doc = Jsoup.connect(str + "/kurs").get();
+        } catch (ConnectException e) {
             System.err.println("Probleme bei der Link-Eingabe");
         }
 
@@ -215,7 +216,7 @@ public class Ariva_Kurs {
         String temp = e.toString();
         //System.out.println(temp2);
 
-        if(temp.contains("keine Kurse vorhanden")){
+        if (temp.contains("keine Kurse vorhanden")) {
             return "boerse_id=";
         }
 
@@ -226,18 +227,16 @@ public class Ariva_Kurs {
         //Hier erstmal rausfinden welches der erste Kurs der Aktie ist, der kein realtime-Kurs ist
         String text;
         int count = 0;
-        for(int i=0;i<101000010;i++){
+        for (int i = 0; i < 101000010; i++) {
             Element e2 = doc.getElementsByClass("ellipsis padding-right-5").get(i);
             text = e2.toString();
-            if(text.contains("mp_realtime")){
+            if (text.contains("mp_realtime")) {
                 count++;
-            }
-            else{
+            } else {
                 break;
             }
         }
         //System.out.println(count);
-
 
 
         Element e3 = doc.getElementsByClass("ellipsis padding-right-5").get(count);
@@ -247,16 +246,16 @@ public class Ariva_Kurs {
         //Ab hier etwas unschön programmiert aber funktioniert!
         //meta zu String umwandeln
         String temp2 = meta.toString();
-        int x=0;
-        int y=0;
+        int x = 0;
+        int y = 0;
 
         //meta-String durchlaufen bis man den Anfang von "boerse_id=.." gefunden hat
-        while(temp2.charAt(x)!='?'){
+        while (temp2.charAt(x) != '?') {
             x++;
         }
         //und bis man das Ende gefunden hat
-        while(y<x){
-            while(temp2.charAt(y)!='"'){
+        while (y < x) {
+            while (temp2.charAt(y) != '"') {
                 y++;
 
             }
@@ -264,7 +263,7 @@ public class Ariva_Kurs {
         }
 
         //Substring ausgeben
-        String temp3 = temp2.substring(x+1, y-1);
+        String temp3 = temp2.substring(x + 1, y - 1);
         //System.out.println(temp3);
 
         return temp3;
@@ -279,38 +278,35 @@ public class Ariva_Kurs {
      * @throws ConnectException
      * @retun WKN
      */
-    public String getWKN(String str) throws IOException{
-        if(str!=null){
+    public String getWKN(String str) throws IOException {
+        if (str != null) {
 
 
-            try{
-                doc = Jsoup.connect("https://www.ariva.de/"+str).get();
-            } catch (ConnectException e){
+            try {
+                doc = Jsoup.connect("https://www.ariva.de/" + str).get();
+            } catch (ConnectException e) {
                 System.err.println("Probleme bei der Link-Eingabe.");
             }
-
 
 
             Element e = doc.getElementsByClass("verlauf snapshotInfo").get(0);
             Element temp = e.child(0);
             String wkn = temp.toString();
             //testen ob überhaupt eine WKN vorhanden ist
-            if(wkn.contains("WKN:")){
-                wkn = wkn.substring(12,18);
+            if (wkn.contains("WKN:")) {
+                wkn = wkn.substring(12, 18);
 
 
                 System.out.println(wkn);
 
 
                 return wkn;
-            }
-            else{
+            } else {
                 System.out.println("Der eingegebene String enthält keine WKN");
                 return "---";
             }
 
-        }
-        else{
+        } else {
             throw new IOException("Stringübergabe fehlgeschlagen");
         }
     }
@@ -321,7 +317,7 @@ public class Ariva_Kurs {
      * @param str sollte später die url einer aktie sein, Verbindung zur feeder klasse
      * @throws IOException
      */
-    public void kursLink(String str) throws IOException{
+    public void kursLink(String str) throws IOException {
 
         String link = str;
 
@@ -334,7 +330,7 @@ public class Ariva_Kurs {
         String teil2;
         String link2;
         teil1 = link.substring(0, i);
-        teil2 = link.substring(i+16, laenge);
+        teil2 = link.substring(i + 16, laenge);
 
 
         Element e = doc.select("label").get(1);
@@ -345,10 +341,10 @@ public class Ariva_Kurs {
 
 
         int j = 1;
-        while(j<(e2/2)){
+        while (j < (e2 / 2)) {
             String erg = e.nextElementSibling().child(j).val();
             String res;
-            res = teil1+"month="+erg+teil2;
+            res = teil1 + "month=" + erg + teil2;
             al.add(res);
 
             j++;
@@ -357,15 +353,12 @@ public class Ariva_Kurs {
         //System.out.println(al.size());
 
 
-
-
-
     }
 
     /**
      * ausgabe der größe der hashmap, in der die key value paare von datum und kurs gespeichert sind
      */
-    public void aus(){
+    public void aus() {
         System.out.println(hm.size());
         System.out.println(hm);
     }
@@ -375,28 +368,27 @@ public class Ariva_Kurs {
      * @param content
      * @return
      */
-    public String mod(String content){
+    public String mod(String content) {
 
-        if(content.contains(".")){
-            content = content.replace(".","");
+        if (content.contains(".")) {
+            content = content.replace(".", "");
         }
 
-        if(content.contains(",")){
-            content = content.replace(",",".");
+        if (content.contains(",")) {
+            content = content.replace(",", ".");
         }
 
         return content;
     }
 
 
-    public String ablauf(String link)throws IOException{
+    public String ablauf(String link) throws IOException {
 
 
         String g = link + "/historische_kurse?" + haupthandelsplatz(link) + "&month=2019-03-31&clean_split=1&clean_split=0&clean_payout=0&clean_bezug=1&clean_bezug=0";
 
         return g;
     }
-
 
 
 }
