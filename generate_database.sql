cdad2c8c76957a87db412dceb0dbd2e3786aa0b3
tobstar97 <37182531+tobstar97@users.noreply.github.com>
2019-04-22 21:16:59 +0200

    Create generate_database.sql

---
+++ generate_database.sql
@@ -0,0 +1,52 @@
+create table aktie
+(
+  ID                  int(10)      null,
+  ISIN                varchar(12)  not null
+    primary key,
+  WKN                 int(6)       null,
+  Branche             varchar(255) null,
+  Aktienname          varchar(255) null,
+  `Aktienlink(ariva)` varchar(255) null,
+  `Index`             varchar(255) null
+);
+
+create table bilanz
+(
+  ID               int(10)      null,
+  ISIN             varchar(12)  not null
+    primary key,
+  Jahr             time         null,
+  Umsatz           double       null,
+  Analystenmeinung varchar(255) null,
+  Gewinn           double       null,
+  EBIT             double       null,
+  Eigenkapital     double       null,
+  Gesamtkapital    double       null,
+  Waehrung         varchar(255) null,
+  letztesUpdate    timestamp    null,
+  constraint bilanz_aktie_ISIN_fk
+  foreign key (ISIN) references aktie (isin)
+);
+
+create table `index`
+(
+  ID        int(10)      null,
+  ISIN      varchar(12)  not null
+    primary key,
+  WKN       int(6)       null,
+  Indexname varchar(255) null,
+  Land      varchar(255) null
+);
+
+create table kurs
+(
+  ID            int(10)      null,
+  ISIN          varchar(12)  not null
+    primary key,
+  Datum         time         null,
+  Kurs          double       null,
+  Waehrung      varchar(255) null,
+  letztesUpdate timestamp    null,
+  constraint kurs_aktie_ISIN_fk
+  foreign key (ISIN) references aktie (isin)
+);
